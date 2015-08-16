9f63d90138aabdeaf9947837139fa6271a97aa57
Danny Andrianto <danny_andrianto@hotmail.com>
2015-08-16 14:26:35 +0700

    Add create product form

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ apotekApp.config(["$routeProvider", function($routeProvider) {
 	$routeProvider.when("/products", {
 		templateUrl: "partials/product-list.html",
 		controller: "ProductListController"
+	}).when("/products/new", {
+		templateUrl: "partials/product-new.html",
+		controller: "ProductNewController"
 	}).otherwise({
 		redirectTo: "/products"
 	});
--- js/controllers.js
+++ js/controllers.js
@@ -2,8 +2,38 @@ var apotekControllers = angular.module("apotekControllers", []);
 var JsobDB = require("node-json-db");
 var db = new JsobDB("data", false, false);
 
-apotekControllers.controller("ProductListController", function($scope) {
-	$scope.products = db.getData("/products");
+apotekControllers.controller("ProductListController", ["$scope", function($scope) {
+	try {
+		$scope.products = db.getData("/products");
+	} catch(error) {
+		$scope.products = [];
+	}
+	console.log($scope.products)
 	
 	$scope.orderProp = "id";
-});
+}]);
+
+apotekControllers.controller("ProductNewController", ["$scope", "$location", function($scope, $location) {
+	$scope.submit = function() {
+		var new_id = 1;
+		try {
+			var products = db.getData("/products");
+			
+			if (products.length > 0) {
+				new_id = products[products.length - 1].id + 1;
+			}
+		} catch(error) {
+			new_id = 1;
+		}
+		
+		db.push("/products[" + (new_id - 1) + "]", {
+			id: parseInt(new_id),
+			name: $scope.name,
+			price: parseInt($scope.price),
+			quantity: parseInt($scope.quantity)
+		});
+		db.save();
+		
+		$location.path("/products");
+	};
+}]);
