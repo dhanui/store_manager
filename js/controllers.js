b2ee2375a06dd5540f17fdef0c5ece3eaff6e9f1
Danny Andrianto <danny_andrianto@hotmail.com>
2015-08-16 08:44:02 +0700

    Use angular js to load data

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+var apotekApp = angular.module("apotekApp", []);
+
+apotekApp.controller("ProductListController", function($scope) {
+	$scope.products = [
+		{
+			"id": 1,
+			"name": "Obat batuk",
+			"price": 10000,
+			"quantity": 50
+		},
+		{
+			"id": 2,
+			"name": "Obat pilek",
+			"price": 9000,
+			"quantity": 75
+		}
+	];
+});
