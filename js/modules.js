5903e7120fd78fadddc20143483d8503c1722444
Danny Andrianto <danny_andrianto@hotmail.com>
2015-11-17 12:07:57 +0700

    Include jquery and bootstrap in modules.js

--- js/modules.js
+++ js/modules.js
@@ -1,3 +1,5 @@
+window.$ = window.jQuery = require("jquery");
+var bootstrap = require("bootstrap");
 var JsonDB = require("node-json-db");
 var angular = require("angular");
 var angular_route = require("angular-route");
